2ed667294ab46adc5b511789937024a0b98759d4
dickey <dickey_lhchan@hotmail.com>
2019-04-16 20:16:31 +0800

    modify create / delete/edit html page

--- server/index.js
+++ server/index.js
@@ -70,24 +70,20 @@ app.delete('/repair_order/:idrepair_order',(req, res)=>{
 
  // insert repair_order
  app.post('/repair_order',(req, res)=>{
-  /*var eid=req.body.idrepair_order;
+  var eid=req.body.idrepair_order;
   var elocation=req.body.location;
   var ebuilding=req.body.building;
   var erequest=req.body.requested_by;
   var econtact=req.body.contact_tel;
-  var edescription=req.body.description;*/
-  var eid=req.body.idrepair_order;
-  var elocation=req.body.location;
-  var ebuilding=req.body.BuildingName;
-  var eday=req.body.day;
-  var erequest=req.body.user;
-  var econtact=req.body.tel;
   var edescription=req.body.description;
+  
+  
   console.log(elocation,ebuilding, erequest, econtact, edescription);
   
-  //var sql="INSERT INTO repair_order (idrepair_order, empName, empAge, post, salary) VALUES (emp.idemployee,emp.empName, emp.empAge, emp.post, emp.salary)";
+   
   var sql="INSERT INTO repair_maint.repair_order (idrepair_order, location, building, requested_by, contact_tel, description) VALUES  ('"+eid+"', '"+elocation+"', '"+ebuilding+"', '"+erequest+"', '"+econtact+"','"+edescription+"')";
   
+ 
  mysqlConnection.query(sql,(err, rows, fields)=>{
     if(!err)
     res.send(rows);
